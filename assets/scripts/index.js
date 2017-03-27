0dc7d49c36851601b115a1abb3b152f1f05cc9a1
SquirtleSquad1988 <alexanderjayrobles0@gmail.com>
2017-03-27 15:54:31 -0400

    Add functional sidebar
    Added sidebar that is animated where when a user clicks on the
    hamburger it pops out of the side. Also added animations in index.js
    and stylings in index.scss. Now I need to update the sidebar to perform
    authorization tasks.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,14 +3,44 @@
 const setAPIOrigin = require('../../lib/set-api-origin');
 const config = require('./config');
 const showNavbar = require('./templates/navbar.handlebars');
+const showSidebar = require('./templates/sidebar.handlebars');
 const handlers = require('./auth/events');
 
 $(() => {
   setAPIOrigin(location, config);
-  $('.navbar-render').append(showNavbar);
+  // $('.navbar-render').append(showSidebar);
   $('#change-password-dropdown').hide();
   $('#exit').hide();
 });
 
 
 $(document).ready(handlers.addHandlers);
+
+$(document).ready(function () {
+  var trigger = $('.hamburger'),
+      overlay = $('.overlay'),
+     isClosed = false;
+
+    trigger.click(function () {
+      hamburger_cross();
+    });
+
+    function hamburger_cross() {
+
+      if (isClosed == true) {
+        overlay.hide();
+        trigger.removeClass('is-open');
+        trigger.addClass('is-closed');
+        isClosed = false;
+      } else {
+        overlay.show();
+        trigger.removeClass('is-closed');
+        trigger.addClass('is-open');
+        isClosed = true;
+      }
+  }
+
+  $('[data-toggle="offcanvas"]').click(function () {
+        $('#wrapper').toggleClass('toggled');
+  });
+});
